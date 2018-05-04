7f80c6ff400b8ae58a722dab9d1a7c657dc830c0
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-04 17:33:59 +0000

    automatically backed up by learn

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(name)
+  def phrase
+    phrase = 'Hi, my name is #{name}.'
+    puts phrase
+end
+  
+def introduction_with_language(name, language)
+  def phrase
+    phrase = 'Hi, my name is #{name} and I am learning to pgoram in #{language}.'
+    puts phrase
+end
